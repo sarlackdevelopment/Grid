b7c9211bbf8c73adb8ca791f14b2c923c3642016
akorobov <akorobov@itquick.ru>
2022-09-09 18:07:54 +0200

    +eslint

--- src/components/grid/index.jsx
+++ src/components/grid/index.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import {Button} from "react-bootstrap";
+import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const Grid = () => <>
     some
     <Button variant='primary'>Some</Button>
-</>
+</>;
 
 export default Grid;
