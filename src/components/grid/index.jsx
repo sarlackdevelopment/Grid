a53368e45709ec3e68aba79f51927304f6545c4b
akorobov <akorobov@itquick.ru>
2022-09-09 18:00:15 +0200

    +react-bootstrap

--- src/components/grid/index.jsx
+++ src/components/grid/index.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import {Button} from "react-bootstrap";
 
-const Grid = () => <>some</>
+const Grid = () => <>
+    some
+    <Button variant='primary'>Some</Button>
+</>
 
 export default Grid;
