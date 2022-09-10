bd9e9408175138ef5a9ecd6a4e0ee367a045cf64
akorobov <akorobov@itquick.ru>
2022-09-10 11:30:25 +0200

    Complete basic dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.scss';
 import Grid from './components/grid';
 
 function App() {
     return (
-        <div className="App">
+        <div>
             <Grid />
         </div>
     );
--- src/components/grid/index.jsx
+++ src/components/grid/index.jsx
@@ -1,28 +1,73 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import cn from 'classnames';
 import {
-    Col, Container, Row
+    Col,
+    Container,
+    Form,
+    Row
 } from 'react-bootstrap';
 import style from './style.module.scss';
 
 const Grid = () => {
-    const DEFAULT_ROWS_COUNT = 21;
-    const DEFAULT_COLS_COUNT = 21;
-    const [rowsCount, setRowsCount] = useState(DEFAULT_ROWS_COUNT);
-    const [colsCount, setColsCount] = useState(DEFAULT_COLS_COUNT);
+    const NAME_ROWS_COUNT_FIELD = 'rowsCountInput';
+    const NAME_COLS_COUNT_FIELD = 'colsCountInput';
+    const DEFAULT_COUNT = 21;
+    const MIN_COUNT = 5;
+    const MAX_COUNT = 100;
+
+    const [rowsCount, setRowsCount] = useState(DEFAULT_COUNT);
+    const [colsCount, setColsCount] = useState(DEFAULT_COUNT);
+    const rows = useMemo(() => [...Array(Number(rowsCount)).keys()], [rowsCount]);
+    const cols = useMemo(() => [...Array(Number(colsCount)).keys()], [colsCount]);
+    const handleChangeCount = (event) => {
+        const { value, id } = event.target;
+        if (id === NAME_ROWS_COUNT_FIELD) {
+            setRowsCount(value);
+        }
+        if (id === NAME_COLS_COUNT_FIELD) {
+            setColsCount(value);
+        }
+    };
     return (
-        <Container className='mt-5'>
+        <Container fluid className='mt-5 mx-2'>
             <Row>
-                <Col>
-                    <Container>Custom</Container>
+                <Col xs={4}>
+                    <Form>
+                        <Form.Group className="mb-3">
+                            <Form.Label htmlFor={NAME_ROWS_COUNT_FIELD}>Rows count:</Form.Label>
+                            <Form.Control
+                                id={NAME_ROWS_COUNT_FIELD}
+                                placeholder="Rows count"
+                                type="number"
+                                max={MAX_COUNT}
+                                min={MIN_COUNT}
+                                onChange={handleChangeCount}
+                                isInvalid={(rowsCount < MIN_COUNT) || (rowsCount > MAX_COUNT)}
+                                defaultValue={DEFAULT_COUNT}
+                            />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label htmlFor={NAME_COLS_COUNT_FIELD}>Cols count:</Form.Label>
+                            <Form.Control
+                                id={NAME_COLS_COUNT_FIELD}
+                                placeholder="Cols count"
+                                type="number"
+                                max={MAX_COUNT}
+                                min={MIN_COUNT}
+                                isInvalid={(colsCount < MIN_COUNT) || (colsCount > MAX_COUNT)}
+                                onChange={handleChangeCount}
+                                defaultValue={DEFAULT_COUNT}
+                            />
+                        </Form.Group>
+                    </Form>
                 </Col>
-                <Col>
-                    {[...Array(colsCount).keys()].map((rowItem) => <div
-                        className='d-flex'
+                <Col xs={8}>
+                    {cols.map((rowItem) => <div
+                        className='d-flex justify-content-center'
                         key={rowItem + rowItem.toString()}
                     >
-                        {[...Array(rowsCount).keys()].map((colItem) => <div
-                            className='d-flex flex-column'
+                        {rows.map((colItem) => <div
+                            className='d-flex flex-column justify-content-center border'
                             key={colItem + colItem.toString()}
                         >
                             <div className={cn(style.cell, {
