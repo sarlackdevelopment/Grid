424cbcf1677df7a21a424ea763545dfd3196b686
akorobov <akorobov@itquick.ru>
2022-09-11 12:57:31 +0200

    bugfix with color

--- src/components/grid/Engine/index.jsx
+++ src/components/grid/Engine/index.jsx
@@ -7,13 +7,13 @@ import { useLogic } from './useLogic';
 
 const Engine = () => {
     const {
-        addFigure, indicatorRef, figures
+        addFigure, figures
     } = useLogic();
     return (<>
         <ModalEditor />
         <Button variant='outline-primary' onClick={addFigure}>Add figure</Button>
         { figures.map(({
-            xCoordinate, yCoordinate, width, height
+            xCoordinate, yCoordinate, width, height, color
         }, index) => <Stack key={index + index.toString()} direction="horizontal" gap={3}>
             <div className='d-flex'>
                 <span className='border px-2'>X: </span>
@@ -31,11 +31,7 @@ const Engine = () => {
                 <span className='border px-2'>W: </span>
                 <span className='border px-3 fw-bold'>{width}</span>
             </div>
-            <div
-                data-key={index + index.toString()}
-                ref={indicatorRef}
-                className={cn('border', style.indicator)}
-            />
+            <div style={{ backgroundColor: color }} className={cn('border', style.indicator)} />
         </Stack>) }
     </>);
 };
--- src/components/grid/Engine/useLogic.js
+++ src/components/grid/Engine/useLogic.js
@@ -1,23 +1,15 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useCallback, useRef } from 'react';
+import { useCallback } from 'react';
 import { useLogic as useModalEditorLogic } from './ModalEditor/useLogic';
 import { selectorFigures } from '../../../redux/engineSlice';
 
 export const useLogic = () => {
-    const indicatorRef = useRef();
     const dispatch = useDispatch();
     const { openEditor } = useModalEditorLogic();
     const figures = useSelector(selectorFigures);
     const addFigure = useCallback(() => dispatch(openEditor), [dispatch, openEditor]);
-    // if (indicatorRef.current) {
-    //     const { key } = indicatorRef.current.dataset;
-    //     const currentIndex = Number(key.slice(0, Number(key.length / 2)));
-    //     console.log(key, currentIndex);
-    //     return figures[currentIndex].color;
-    // }
     return {
         openEditor,
-        indicatorRef,
         figures,
         addFigure
     };
